6802dae8e71e63255f38af5ab97a6b14a749dca3
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-23 18:41:25 -0300

    refactor: add de middleware de erros

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express"
-import entregasRouter from "./src/router/entregas.routes.js"
+import entregasRouter from "./src/routes/entregas.routes.js"
+import { errorMiddleware } from "./src/middlewares/error.middleware.js"
 
 const app = express()
 app.use(express.json())
 
-app.use("/entregas", entregasRouter)
+app.use("/api/entregas", entregasRouter)
+
+app.use(errorMiddleware)
 
 export default app
