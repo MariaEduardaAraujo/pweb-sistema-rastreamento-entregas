5b78328f9e850f05232ee2906c2167bb5afc65cf
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-30 17:01:13 -0300

    fix: editando caminho da classe entregas.repository

--- src/routes/entregas.routes.js
+++ src/routes/entregas.routes.js
@@ -1,21 +1,21 @@
 import { Router } from "express"
 import { Database } from "../database/database.js"
-import { EntregasRepository } from "../repositories/entregas.repositories.js"
+import { EntregasRepository } from "../repositories/entregas.repository.js"
 import { EntregasService } from "../services/entregas.service.js"
 import { EntregasController } from "../controllers/entregas.controller.js"
 
 const database = new Database()
-const repository = new EntregasRepository(database)
-const service = new EntregasService(repository)
-const controller = new EntregasController(service)
+const entregasRepository = new EntregasRepository(database)
+const entregasService = new EntregasService(entregasRepository, motoristasRepository)
+const entregasController = new EntregasController(entregasService)
 
 const router = Router()
 
-router.get("/", controller.listarTodos)
-router.get("/:id", controller.buscarPorId)
-router.post("/", controller.criar)
-router.patch("/:id/avancar", controller.avancar)
-router.patch("/:id/cancelar", controller.cancelar)
-router.get("/:id/historico", controller.historico)      
+router.get("/", entregasController.listarTodos)
+router.get("/:id", entregasController.buscarPorId)
+router.post("/", entregasController.criar)
+router.patch("/:id/avancar", entregasController.avancar)
+router.patch("/:id/cancelar", entregasController.cancelar)
+router.get("/:id/historico", entregasController.historico)
 
 export default router
