59d5acd45ec411dd58b61899ae7037a8ddda2458
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-30 08:44:19 -0300

    feat: criando interface IMotoristasRepository

---
+++ src/interfaces/IMotoristasRepository.js
@@ -0,0 +1,17 @@
+export class IMotoristasRepository{
+    async listarTodos() {
+        throw new Error("Método listarTodos() não implementado")
+    }
+    async buscarPorId(id) {
+        throw new Error("Método buscarPorId() não implementado")
+    }
+    async buscarPorCPF(cpf) {
+        throw new Error("Método buscarPorCPF() não implementado")
+    }
+    async criar(dados) {
+        throw new Error("Método criar() não implementado")
+    }
+    async atualizar(id, dados) {
+        throw new Error("Método atualizar() não implementado")
+    }
+}
