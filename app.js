d031fd70ab304a3229d37bec9749632e314474a3
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-31 08:36:43 -0300

    feat: adicionando router de motoristas

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import entregasRouter from "./src/routes/entregas.routes.js"
+import motoristasRouter from "./src/routes/motoristas.routes.js"
 import { errorMiddleware } from "./src/middlewares/error.middleware.js"
 
 const app = express()
 app.use(express.json())
 
 app.use("/api/entregas", entregasRouter)
+app.use("/api/motoristas", motoristasRouter)
 
 app.use(errorMiddleware)
 
